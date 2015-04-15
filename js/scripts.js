455b5460a96716f6372d5b0b60d78bed873bd015
geoff winner & marc lee <student@epicodus.com>
2015-04-15 11:30:51 -0700

    add jquery and js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,48 @@ var BankAccount = {
     balance: 0,
     withdraw: function(amount) {
     this.balance = this.balance - amount;
+    return this.balance;
     },
     deposit: function(amount) {
     this.balance = this.balance + amount;
+    return this.balance;
     }
 };
 
 
 $(document).ready(function() {
-    $('form#bank').submit(function() {
-        var name('input#name').val();
-        var amount('input#amount').val();
-        var balance = BankAccount(amount);
-        
+    var myBankAccount = Object.create(BankAccount);
+    $('form#create').submit(function(event) {
+        var name = $('input#name').val();
+        var amount = parseInt($('input#inideposit').val());
+        myBankAccount.deposit(amount);
+        $("#create").hide();
+        $('.withdraw').show();
+        $('.name').text(name);
+        $('.inideposit').text(myBankAccount.balance);
+
+
+        event.preventDefault();
+
+
+    });
+    $('form#withdep').submit(function(event){
+        event.preventDefault();
+
+
+        var depamount = parseInt($('#deposit').val());
+        if (!depamount) {
+            depamount = 0;
+        };
+        myBankAccount.deposit(depamount);
+        $('.inideposit').text(myBankAccount.balance);
+
+        var withamount = parseInt($('#withdrawal').val());
+            if (!withamount) {
+                withamount = 0;
+            };
+        myBankAccount.withdraw(withamount);
+        $('.inideposit').text(myBankAccount.balance);
 
 
     });
